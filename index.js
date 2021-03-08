8a90793289647a068a6d378c54e763a0a7fe91b8
Mihail Calaras <mihail.calaras@reigncode.dev>
2021-03-08 23:55:06 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Joi = require('joi');
+const logger = require('./middleware/logger');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('config');
+const debug = require('debug')('app:startup');
+const app = express();
+const courses = require('./routes/courses');
+const home = require('./routes/home');
+
+app.set('view engine', 'pug');
+app.set('views', './views');  // default
+
+app.use(express.json());  // req.body
+app.use(express.urlencoded({ extended: true }));  // key=value&key=value
+app.use(express.static('public'));
+app.use(helmet());
+app.use('/api/courses', courses);
+app.use('/', home);
+
+//Configuration
+console.log('Application name: ' + config.get('name'));
+console.log('Mail server: ' + config.get('mail.host'));
+console.log('Mail password: ' + config.get('mail.password'));
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    debug('Morgan enabled...');
+}
+
+app.use(logger);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
